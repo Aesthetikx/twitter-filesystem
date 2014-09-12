ea96b00f0ac9e20b78204eacd906b06df0ba71f0
John DeSilva <desilvjo@umich.edu>
2014-09-11 22:14:07 -0400

    Add compression object

---
+++ src/main/scala/Compression.scala
@@ -0,0 +1,13 @@
+package com.aesthetikx.twitterfilesystem
+
+object Compression {
+
+  def compress(data: String): List[Byte] = {
+
+  }
+
+  def split(data: List[Byte]): List[String] = {
+
+  }
+
+}
