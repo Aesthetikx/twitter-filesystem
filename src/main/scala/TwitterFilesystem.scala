a7579e4300b1f4530239aa8c10f1f8bec36e348a
John DeSilva <desilvjo@umich.edu>
2014-09-19 01:45:02 -0400

    Add initial main

--- src/main/scala/TwitterFilesystem.scala
+++ src/main/scala/TwitterFilesystem.scala
@@ -1,5 +1,12 @@
 package com.aesthetikx.twitterfilesystem
 
+import scalaj.http.Token
+
 object TwitterFilesystem {
+
+  def main(argv: Array[String]) {
+    val token: Token = Authentication.loadToken.getOrElse(Authentication.userAuth())
+    //println(Twitter.postTweet(token, "Test Tweet"))
+  }
 
 }
